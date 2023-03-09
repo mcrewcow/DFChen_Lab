484ccc4332c68b42a56831abe30c80fe98c2de95
Emil Kryukov <77118598+mcrewcow@users.noreply.github.com>
2023-03-09 13:57:10 -0500

    Update h5ad_to_h5seurat_p2.R

--- h5ad_to_h5seurat_p2.R
+++ h5ad_to_h5seurat_p2.R
@@ -1,5 +1,6 @@
 library(Seurat)
 
+#Read the counts
 message("Reading counts...")
 x <- read.csv("raw-counts.csv",header=TRUE)
 rownames(x) <- x[,1]
@@ -7,6 +8,7 @@ x[,1] <- NULL
 print(dim(x))
 print(x[1:5,1:5])
 
+#Read the metadata
 message("Reading metadata...")
 m <- read.csv("metadata.csv",header=TRUE)
 rownames(m) <- m[,1]
@@ -14,6 +16,7 @@ colnames(m)[1] <- "sample"
 print(dim(m))
 print(head(m))
 
+#Create and save Seurat object, could be changed to .h5Seurat
 message("Writing seurat object...")
 saveRDS(
   CreateSeuratObject(counts=t(x),meta.data=m,project="seurat",min.cells=0,min.features=0),
